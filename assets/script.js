f958fcf5f5dcefbb2101e0107934fb559c174625
Taani Maama <taani1maama@live.com>
2020-10-19 18:23:33 -0700

    I switched the writePassword in one of the functions to generate password. Added some pseudocode. to help me navigate.

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,24 @@
 // Assignment Codes
 var generateBtn = document.querySelector("#generate");
-let pwLength;
-let useLower;
-let useUpper;
-let useNumbers;
-let useSymbols;
 
 // Generator Functions - these are going to help us create the passwords by having these functions pull from a random number and associating it with the correct string on the Character Code. 
 let useLower = function randomLowerCase() {
   // Here I have the function returning us a random string from the CharCode from 97 up to 122. This will out put letters a-z in lower case.
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
-console.log(randomLowerCase())
+console.log(useLower())
 
 let useUpper = function randomUpperCase() {
   // Here I have the function returning us a random string from the CharCode from 65 up to 90. This will out put letters A-Z in upper case.
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
-console.log(randomUpperCase())
+console.log(useUpper())
 
 let useNumbers = function randomNumber() {
   // Like the others this function pulls out a random number from the CharCode provided.
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
-console.log(randomNumber())
+console.log(useNumbers())
 
 let useSymbols = function randomSymbol() {
   // We needed to use concatenations so we could bring multiple strings together because in the char code the symbols are broken up into 4 different decoder ranges.
@@ -35,31 +30,15 @@ let useSymbols = function randomSymbol() {
     String.fromCharCode(Math.floor(Math.random() * 4) + 123)
   )
 
-  console.log(randomSym)
   let randomIndex = (Math.floor(Math.random() * randomSym.length))
   return randomSym[randomIndex]
 }
-console.log(useSymbols)
+console.log(useSymbols())
+
 // The charCode that I am referencing can be found here https://www.w3schools.com/charsets/ref_utf_basic_latin.asp
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var passwordEL = document.getElementById('password');
-
-  passwordText.value = password;
-  password = '';
-    
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword()); {
-
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-
-function writePassword() {
+function generatePassword() {
+  // Password Length
   function characterAmount() {
     var pwLength = parseInt(prompt('Please choose from 8 to 128 characters.'))
     
@@ -73,24 +52,51 @@ function writePassword() {
 
     console.log(pwLength)
   }
+
+  // To include lower case or not.
   function includeLowerCase() {
     useUpper = confirm('Include lower case letters?')
     if (useLower = true) {
 
     }
-    else 
+    else {}
   }
+
+  // To include upper case letters or not.
   function includeUpperCase() {
     confirm('Include upper case letters?')
   }
+
+  // To include numbers or not.
   function includeNumbers() {
     confirm('Include numbers?')
   }
+
+  // To include symbols or not.
   function includeSymbols() {
     confirm('Include symbols?')
   }
 }
 }
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  var passwordEL = document.getElementById('password');
+
+  passwordText.value = password;
+  password = '';
+    
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword); {
+
+// GIVEN I need a new, secure password
+// WHEN I click the button to generate a password
+
+
 // THEN I am presented with a series of prompts for password criteria
 // WHEN prompted for password criteria
 // THEN I select which criteria to include in the password
